5759c9c67ece29bfccf9aae99e770f6d7cab140a
Jacob Ilias Komissar <jikomissar@wpi.edu>
2019-02-18 19:50:56 -0500

    Use material colors in model.js

--- src/model.js
+++ src/model.js
@@ -16,50 +16,58 @@ const sphere = new Mesh(_sphere.vertices.map(vec3),
 const cube = new Mesh(_cube.vertices.map(vec3),
                       _cube.faces);
 
-let white = [1, 1, 1, 1],
-    black = [0, 0, 0, 1],
-    gray = [0.5, 0.5, 0.5, 1],
-    red = [1, 0, 0, 1],
-    green = [0, 1, 0, 1],
-    blue = [0, 0, 1, 1],
-    cyan = [0, 1, 1, 1],
-    magenta = [1, 0, 1, 1],
-    yellow = [1, 1, 0, 1],
-    pale_purple = [0.5, 0.5, 1, 1];
+let white = [1, 1, 1],
+    black = [0, 0, 0],
+    gray = [0.5, 0.5, 0.5],
+    red = [1, 0, 0],
+    green = [0, 1, 0],
+    blue = [0, 0, 1],
+    cyan = [0, 1, 1],
+    magenta = [1, 0, 1],
+    yellow = [1, 1, 0],
+    pale_purple = [0.5, 0.5, 1];
 
 
-let root = Mobile.builder(cube, pale_purple)
+let root = Mobile.builder(cube)
+    .color(pale_purple)
+    .shininess(100)
     .radius(4)
     .parentHeight(10)
     .childHeight(0.5)
     .spinSpeed(0.06)
     .armSpeed(0.05),
 
-    L = root.left(sphere, red)
+    L = root.left(sphere)
+    .color(red)
     .parentHeight(0.5)
     .spinSpeed(0.125)
     .armSpeed(0.075),
 
-    R = root.right(cube, green)
+    R = root.right(cube)
+    .color(green)
     .parentHeight(2)
     .childHeight(0.5)
     .spinSpeed(0.075)
     .armSpeed(0.125),
 
-    ll = L.left(cube, cyan)
+    ll = L.left(cube)
+    .color(cyan)
     .parentHeight(1.25)
     .spinSpeed(0.1)
     .armSpeed(0),
 
-    lr = L.right(sphere, yellow)
+    lr = L.right(sphere)
+    .color(yellow)
     .radius(0)
     .childHeight(0.25)
     .spinSpeed(0.5),
 
-    lrl = lr.left(cube.scaled(0.5), gray)
+    lrl = lr.left(cube.scaled(0.5))
+    .color(gray)
     .spinSpeed(0.1),
 
-    rl = R.left(sphere, magenta)
+    rl = R.left(sphere)
+    .color(magenta)
     .parentHeight(0.5)
     .spinSpeed(0.025),
 
@@ -69,8 +77,12 @@ let root = Mobile.builder(cube, pale_purple)
     .childHeight(3)
     .armSpeed(0.1),
 
-    rrl = rr.left(sphere.scaled(0.25), blue).spinSpeed(0.5),
-    rrr = rr.right(cube.scaled(0.25), white).spinSpeed(0.05);
+    rrl = rr.left(sphere.scaled(0.25))
+    .color(blue)
+    .spinSpeed(0.5),
+    rrr = rr.right(cube.scaled(0.25))
+    .color(white)
+    .spinSpeed(0.05);
 
 export const mobile = root.build();
 
