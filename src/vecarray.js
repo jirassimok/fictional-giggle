05018c2b65fb058db0d3ab1c9e530929838effba
Jacob Ilias Komissar <jikomissar@wpi.edu>
2019-02-21 13:51:25 -0500

    Allow setting of x, y, and z on arrays

--- src/vecarray.js
+++ src/vecarray.js
@@ -5,16 +5,25 @@ Object.defineProperties(Array.prototype, {
     x: {
         get: function () {
             return this[0];
+        },
+        set: function(val) {
+            return this[0] = val;
         }
     },
     y: {
         get: function () {
             return this[1];
+        },
+        set: function(val) {
+            return this[1] = val;
         }
     },
     z: {
         get: function () {
             return this[2];
+        },
+        set: function(val) {
+            return this[2] = val;
         }
     }
 });
